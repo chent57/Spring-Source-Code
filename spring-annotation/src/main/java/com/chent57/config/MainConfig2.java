720cfef1c8cb26272695f8e123d73a516ed47d17
chent57 <chent1993@163.com>
2020-08-06 23:46:00 +0800

    @Lazy

--- spring-annotation/src/main/java/com/chent57/config/MainConfig2.java
+++ spring-annotation/src/main/java/com/chent57/config/MainConfig2.java
@@ -3,12 +3,15 @@ package com.chent57.config;
 import com.chent57.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MainConfig2 {
 
-    @Scope("prototype")
+    //@Scope("prototype")
+
+    @Lazy // 单实例bean默认在容器启动的时候创建对象；当设置为懒加载时，容器启动不创建对象，第一次使用/获取bean再创建对象并初始化
     @Bean("person")
     public Person person() {
         System.out.println("给容器中添加Person");
--- spring-annotation/src/test/java/com/chent57/test/IOCTest.java
+++ spring-annotation/src/test/java/com/chent57/test/IOCTest.java
@@ -24,10 +24,10 @@ public class IOCTest {
     @Test
     public void test02() {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig2.class);
-        String[] beanDefinition = applicationContext.getBeanDefinitionNames();
-        for (String name : beanDefinition) {
-            System.out.println(name);
-        }
+//        String[] beanDefinition = applicationContext.getBeanDefinitionNames();
+//        for (String name : beanDefinition) {
+//            System.out.println(name);
+//        }
         // 默认是单例的，ioc容器启动会调用方法(构造函数)创建对象放到ioc容器中，以后每次获取直接从容器中拿（map.get()）
         // 多实例（用@Scope("prototype)设置）：ioc容器启动不会调用方法创建对象放到容器中，每次获取的时候才会调用方法创建对象放到ioc容器中
         System.out.println("ioc容器创建完成");
