477da343bf1f50c3c9c758313665eda58aec623a
chent57 <chent1993@163.com>
2021-11-16 21:27:58 +0800

    modify build.gradle

--- src/test/java/com/chent57/test/IOCTest_AOP.java
+++ src/test/java/com/chent57/test/IOCTest_AOP.java
@@ -6,19 +6,24 @@ import com.chent57.bean.Car;
 import com.chent57.bean.Color;
 import com.chent57.config.MainConfigOfAOP;
 import com.chent57.config.MainConfigOfAutowired;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class IOCTest_AOP {
 
     @Test
     public void test01() {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
+        try {
+            AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
 
-        MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
-        mathCalculator.div(1, 0);
+            MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
+            mathCalculator.div(1, 0);
+            applicationContext.close();
+        } catch (Exception e) {
+            log.error("exception", e);
+        }
 
-
-        applicationContext.close();
     }
 }
