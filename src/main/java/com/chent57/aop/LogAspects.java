b4f5929bf56eff1fdefe90a549ed55da0de85667
chent57 <chent1993@163.com>
2021-11-16 22:59:25 +0800

    general fix

--- src/main/java/com/chent57/aop/LogAspects.java
+++ src/main/java/com/chent57/aop/LogAspects.java
@@ -6,20 +6,28 @@ import org.springframework.core.annotation.AliasFor;
 
 import java.util.Arrays;
 
-/*
-* 切面类
-* @Aspect：告诉Spring当前类是一个切面类
-* */
+/**
+ * 切面类
+ * @author trentchen
+ * @Aspect：告诉Spring当前类是一个切面类
+ */
 @Aspect
 public class LogAspects {
-    // 抽取公共的切入点表达式
-    // 1. 如果在本类引用
-    // 2. 其他的切面引用，则使用全名（com.chent57.aop.LogAspects.pointCut）
+
+
+    /**
+     * 抽取公共的切入点表达式
+     * 1. 如果在本类引用
+     * 2. 其他的切面引用，则使用全名（com.chent57.aop.LogAspects.pointCut）
+     */
     @Pointcut("execution(public int com.chent57.aop.MathCalculator.*(..))")
     public void pointCut() {};
 
 
-    // @Before:在目标方法之前切入；切入点表达式（指定在那个方法切入）,指定类的所有方法用*,两个点表示不指定参数
+    /**
+     * @Before:在目标方法之前切入；切入点表达式（指定在那个方法切入）,指定类的所有方法用*,两个点表示不指定参数
+     * @param joinPoint jointpoint
+     */
     @Before("pointCut()")
     public void logStart(JoinPoint joinPoint) { // JoinPoint必须在参数表第一位
         System.out.println("@Before 除法开始！");
--- src/main/java/com/chent57/aop/MathCalculator.java
+++ src/main/java/com/chent57/aop/MathCalculator.java
@@ -1,5 +1,8 @@
 package com.chent57.aop;
 
+/**
+ * @author trentchen
+ */
 public class MathCalculator {
     public int div(int i, int j) {
         System.out.println("除法函数被调用！"); // 这是一种耦合的方式
